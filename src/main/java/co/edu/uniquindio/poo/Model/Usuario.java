ae441e46ec41b454d5429de7d8a52bf0102b1621
michaelj305 <maicol.alvarezuwu@gmail.com>
2025-03-23 22:55:27 -0500

    se termino la clase repositorio contacto

---
+++ src/main/java/co/edu/uniquindio/poo/Model/Usuario.java
@@ -0,0 +1,5 @@
+package co.edu.uniquindio.poo.Model;
+
+public class Usuario {
+    
+}
--- src/main/java/co/edu/uniquindio/poo/gestorcontactos/Repositorios/RepositorioContacto.java
+++ src/main/java/co/edu/uniquindio/poo/gestorcontactos/Repositorios/RepositorioContacto.java
@@ -1,4 +1,46 @@
 package co.edu.uniquindio.poo.gestorcontactos.Repositorios;
 
-public class RepositorioContacto {
+import co.edu.uniquindio.poo.gestorcontactos.Model.Contacto;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class RepositorioContacto implements RepositorioInterfaceUsuario {
+
+private ObservableList<Contacto> contactos;
+    public RepositorioContacto() {
+        this.contactos = FXCollections.observableArrayList();
+    }
+
+    @Override
+    public void agregarContacto(Contacto contacto) {
+        contactos.add(contacto);
+        
+    }
+
+    @Override
+    public void eliminarContacto(Contacto contacto) {
+        contactos.remove(contacto);
+        
+    }
+
+    @Override
+    public Contacto obtenerContactoTelefono(String telefono) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ObservableList<Contacto> obtenerContactos() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ObservableList<Contacto> obtenerContactosNombre(String nombre) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    
 }
+
