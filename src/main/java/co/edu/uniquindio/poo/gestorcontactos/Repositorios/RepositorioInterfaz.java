d350b985be3f4080df5a5087601e5910dd017dc8
serrrrrgio <sergio.posadag@uqvirtual.edu.co>
2025-03-23 22:17:01 -0500

    se crea RepositorioInterfaz

---
+++ src/main/java/co/edu/uniquindio/poo/gestorcontactos/Repositorios/RepositorioInterfaz.java
@@ -0,0 +1,9 @@
+package co.edu.uniquindio.poo.gestorcontactos.Repositorios;
+
+import co.edu.uniquindio.poo.gestorcontactos.Model.Contacto;
+import javafx.collections.ObservableList;
+
+public class RepositorioInterfaz {
+
+
+}
