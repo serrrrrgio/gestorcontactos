957519ecd2ec9ea61c8ed5efd9e146a2ce976ea1
serrrrrgio <sergio.posadag@uqvirtual.edu.co>
2025-03-23 22:00:58 -0500

    Se termina la clase contacto

--- src/main/java/co/edu/uniquindio/poo/gestorcontactos/Model/Contacto.java
+++ src/main/java/co/edu/uniquindio/poo/gestorcontactos/Model/Contacto.java
@@ -1,4 +1,17 @@
 package co.edu.uniquindio.poo.gestorcontactos.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.MonthDay;
+
+@Setter
+@Getter
+@AllArgsConstructor
 public class Contacto {
+    private String nombre;
+    private String  apellido;
+    private String telefono;
+    private MonthDay fechaCumplanos;
 }
--- src/main/java/co/edu/uniquindio/poo/gestorcontactos/Model/Usuario.java
+++
@@ -1,4 +0,0 @@
-package co.edu.uniquindio.poo.gestorcontactos.Model;
-
-public class Usuario {
-}
