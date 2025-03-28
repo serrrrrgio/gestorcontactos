e1100bfb1e3e2f30a4692318556755cd84f70a35
michaelj305 <maicol.alvarezuwu@gmail.com>
2025-03-27 20:04:05 -0500

    cambios en el fxml y en el module

--- src/main/java/co/edu/uniquindio/poo/gestorcontactos/Controller/InicioUsuarioController.java
+++ src/main/java/co/edu/uniquindio/poo/gestorcontactos/Controller/InicioUsuarioController.java
@@ -1,4 +1,58 @@
 package co.edu.uniquindio.poo.gestorcontactos.Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class InicioUsuarioController {
+
+    @FXML
+    private TableColumn<?, ?> lblNombre;
+
+    @FXML
+    private TextField txtCorreo;
+
+    @FXML
+    private TableColumn<?, ?> lblApellido;
+
+    @FXML
+    private TableColumn<?, ?> lblTelefono;
+
+    @FXML
+    private TableColumn<?, ?> lblCumpleanos;
+
+    @FXML
+    private TableView<?> tblContactos;
+
+    @FXML
+    private TextField txtNombre;
+
+    @FXML
+    private TableColumn<?, ?> lblCorreo;
+
+    @FXML
+    private Button btnEliminar;
+
+    @FXML
+    private Button btnAnadirFoto;
+
+    @FXML
+    private TextField txtTelefono;
+
+    @FXML
+    private DatePicker dateFechaCumplanos;
+
+    @FXML
+    private TextField txtApellido;
+
+    @FXML
+    private Button btnAnadir;
+
+    @FXML
+    private Button btnActualizar;
+
 }
+
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module co.edu.uniquindio.poo.gestorcontactos {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens co.edu.uniquindio.poo.gestorcontactos to javafx.fxml;
     exports co.edu.uniquindio.poo.gestorcontactos;
 }
+
